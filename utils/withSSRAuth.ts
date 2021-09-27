a3f86fa04847fd3d1b2bef9ee0613dab16e314ce
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-27 12:28:42 -0300

    Added JwtDecode and Server Side Validation

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,3 +1,4 @@
+import jwtDecode from 'jwt-decode'
 import {
   GetServerSideProps,
   GetServerSidePropsContext,
@@ -5,14 +6,24 @@ import {
 } from 'next'
 import { destroyCookie, parseCookies } from 'nookies'
 import { AuthTokenError } from '../services/errors/AuthTokenError'
+import { validateUserPermissions } from './validateUserPermissions'
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type WithSSRAuthOptions = {
+  permissions?: string[]
+  roles?: string[]
+}
+
+export function withSSRAuth<P>(
+  fn: GetServerSideProps<P>,
+  options?: WithSSRAuthOptions
+): GetServerSideProps {
   return async (
     ctx: GetServerSidePropsContext
   ): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx)
+    const token = cookies['nextauth.token']
 
-    if (!cookies['nextauth.token']) {
+    if (!token) {
       return {
         redirect: {
           destination: '/',
@@ -20,6 +31,27 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
         }
       }
     }
+
+    if (options) {
+      const user = jwtDecode<{ permissions: string[]; roles: string[] }>(token)
+
+      const { permissions, roles } = options
+
+      const userHasValidPermissions = validateUserPermissions({
+        user,
+        permissions,
+        roles
+      })
+      if (!userHasValidPermissions) {
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false
+          }
+        }
+      }
+    }
+
     try {
       return await fn(ctx)
     } catch (error) {
