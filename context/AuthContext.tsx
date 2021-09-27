a971422694990ebc4f2d7a160564a6fd71bdf7e1
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-09-27 08:47:20 -0300

    Added Axios Request Queue

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -52,7 +52,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         maxAge: 60 * 60 * 24 * 30,
         path: '/'
       })
-      setCookie(undefined, 'nextauth.token', refreshToken, {
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
         maxAge: 60 * 60 * 24 * 30,
         path: '/'
       })
